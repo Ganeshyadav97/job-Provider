080b6b7be41490bf9b91a47bb8c4a430405876cd
Ganeshyadav97 <166576341+Ganeshyadav97@users.noreply.github.com>
2025-10-26 12:25:50 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use("/user", UserApply);
 
 // ✅ Connect to MongoDB
 mongoose
-  .connect("mongodb+srv://kanneboina:nani@cluster0.wl6wuhf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  .connect("mongodb+srv://kanneboina:nani@cluster0.wl6wuhf.mongodb.net/?appName=Cluster0")
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
